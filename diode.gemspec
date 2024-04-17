5a2ebc7787488a8fa256ab65eb8ff5b3749c947b
kjellhex <kjell@null4.net>
2024-04-18 06:57:10 +1000

    removed dependency on async-io, replace with async, simplify with explicit fiber scheduling

--- diode.gemspec
+++ diode.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "diode"
-  s.version     = "1.2.1"
+  s.version     = "1.3.0"
   s.summary     = "A fast, simple, pure-ruby application server."
   s.description = "Diode helps you to build REST application servers by providing a container for your servlets.
   About as simple as rack but more powerful, and nowhere near as complex as passenger, Diode has only four classes
@@ -10,6 +10,7 @@ Gem::Specification.new do |s|
   s.files       = ["request.rb","response.rb","server.rb","static.rb"].collect{|f| "lib/diode/#{f}"}
   s.homepage    = "https://github.com/kjellhex/diode"
   s.license     = "MIT"
-  s.add_dependency "json"
-  s.add_dependency "async-io"
+  s.required_ruby_version = '>= 3.2.0'
+  s.add_runtime_dependency 'json', '~> 2'
+  s.add_runtime_dependency 'async', '~> 2'
 end
--- lib/diode/response.rb
+++ lib/diode/response.rb
@@ -4,54 +4,54 @@ module Diode
 
 class Response
 
-	STATUS = {
-		200 => "OK",
-		400 => "Bad Request",
-		403 => "Forbidden",
-		404 => "Not Found",
-		405 => "Method Not Allowed"
-	}
+  STATUS = {
+    200 => "OK",
+    400 => "Bad Request",
+    403 => "Forbidden",
+    404 => "Not Found",
+    405 => "Method Not Allowed"
+  }
 
-	DEFAULT_HEADERS = {
-		"Content-Type" => "application/json",
-		"Date" => Time.now.rfc2822(),
-		"Cache-Control" => "no-store",
-		"Server" => "Diode/1.0",
-		"Connection" => "Keep-Alive"
-	}
+  DEFAULT_HEADERS = {
+    "Content-Type" => "application/json",
+    "Date" => Time.now.httpdate(),
+    "Cache-Control" => "no-store",
+    "Server" => "Diode/1.0",
+    "Connection" => "Keep-Alive"
+  }
 
-	# returns the html for a few status codes
-	def self.standard(code)
-		raise("code #{code} is not supported") unless STATUS.key?(code)
-		message = STATUS[code]
-		body = "<html><head><title>#{message}</title></head><body><h1>#{code} - #{message}</h1></body></html>\n"
-		h = DEFAULT_HEADERS.merge({"Content-Type" => "text/html"})
-		new(code, body, h)
-	end
+  # returns the html for a few status codes
+  def self.standard(code)
+    raise("code #{code} is not supported") unless STATUS.key?(code)
+    message = STATUS[code]
+    body = "<html><head><title>#{message}</title></head><body><h1>#{code} - #{message}</h1></body></html>\n"
+    h = DEFAULT_HEADERS.merge({"Content-Type" => "text/html"})
+    new(code, body, h)
+  end
 
-	def self.xml(code, body="", headers={})
-		resp = Response.new(code, body, headers)
-		resp.headers["Content-Type"] = "application/xml"
-		return resp
-	end
+  def self.xml(code, body="", headers={})
+    resp = Response.new(code, body, headers)
+    resp.headers["Content-Type"] = "application/xml"
+    return resp
+  end
 
-	attr_accessor(:code, :body, :headers)
+  attr_accessor(:code, :body, :headers)
 
-	def initialize(code, body="", headers={})
-		@code = code.to_i()
-		@body = body
-		@headers = DEFAULT_HEADERS.merge(headers)
-	end
+  def initialize(code, body="", headers={})
+    @code = code.to_i()
+    @body = body
+    @headers = DEFAULT_HEADERS.merge(headers)
+  end
 
-	# return the response as a raw HTTP string
-	def to_s()
-		@headers["Content-Length"] = @body.bytes.size() unless @body.empty?
-		msg = ["HTTP/1.1 #{@code} #{STATUS[@code]}"]
-		@headers.keys.each { |k|
-			msg << "#{k}: #{@headers[k]}"
-		}
-		msg.join("\r\n") + "\r\n\r\n" + @body
-	end
+  # return the response as a raw HTTP string
+  def to_s()
+    @headers["Content-Length"] = @body.bytes.size() unless @body.empty?
+    msg = ["HTTP/1.1 #{@code} #{STATUS[@code]}"]
+    @headers.keys.each { |k|
+      msg << "#{k}: #{@headers[k]}"
+    }
+    msg.join("\r\n") + "\r\n\r\n" + @body
+  end
 
 end
 end
